b25066925fa7213b5746f0918d0446a698a401b9
pranjaalg <143337693+pranjaalg@users.noreply.github.com>
2025-04-24 12:36:02 +0545

    API for CRUD operations

---
+++ src/models/User.ts
@@ -0,0 +1,53 @@
+interface UserData {
+    firstname: string;
+    lastname: string;
+  }
+  
+  interface UserModel extends UserData {
+    id: number;
+  }
+  
+  // Simple in-memory data store
+  class User {
+    private static users: UserModel[] = [];
+    private static nextId: number = 1;
+    
+    static findAll(): UserModel[] {
+      return this.users;
+    }
+    
+    static findById(id: string): UserModel | undefined {
+      return this.users.find(user => user.id === parseInt(id));
+    }
+    
+    static create(userData: UserData): UserModel {
+      const newUser: UserModel = {
+        id: this.nextId++,
+        firstname: userData.firstname,
+        lastname: userData.lastname
+      };
+      
+      this.users.push(newUser);
+      return newUser;
+    }
+    
+    static update(id: string, userData: Partial<UserData>): UserModel | null {
+      const user = this.findById(id);
+      
+      if (!user) return null;
+      
+      if (userData.firstname) user.firstname = userData.firstname;
+      if (userData.lastname) user.lastname = userData.lastname;
+      
+      return user;
+    }
+    
+    static delete(id: string): boolean {
+      const initialLength = this.users.length;
+      this.users = this.users.filter(user => user.id !== parseInt(id));
+      
+      return this.users.length !== initialLength;
+    }
+  }
+  
+  export default User;
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+const usersRoutes = require('./routes/users');
+
+const app: Application = express();
+const PORT: number =  3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', usersRoutes);
+
+// Home route
+app.get('/', (req: Request, res: Response): void => {
+  res.send('API endpoints');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
